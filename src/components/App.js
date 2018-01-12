15a1bf8e7150d97d8062ed7bbe7c7a4bbc5ee981
Kevin Mudiandambo <Kevin.Mudiandambo@nisc.coop>
2018-01-12 16:05:45 -0600

    adding components for home, header, and footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 
+import { BrowserRouter, Route } from 'react-router-dom';
+
+// COMPONENTS
+import Header from './header';
+import Footer from './footer';
+import Home from './home';
+
 class App extends Component {
     render() {
         return (
-            <div>NBA Highlights</div>
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    <Route exact path="/" component={Home} />
+                    <Footer />
+                </div>
+            </BrowserRouter>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 
 
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
