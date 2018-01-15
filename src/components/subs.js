76580c0e8d930425e0f619938b22450fff920909
heero7 <kevin7.mudia@gmail.com>
2018-01-15 11:15:21 -0600

    can add email to subscription list

--- src/components/subs.js
+++ src/components/subs.js
@@ -5,7 +5,9 @@ class Subscriptions extends Component {
         super(props);
 
         this.state = {
-            email: ''
+            email: '',
+            error: false,
+            success: false
         }
     }
 
@@ -15,9 +17,43 @@ class Subscriptions extends Component {
         });
     }
 
+    saveSubscription(email) {
+        const URL_EMAIL = 'http://localhost:3004/subscriptions';
+        fetch(URL_EMAIL, {
+            method: 'POST',
+            headers: {
+                'Accept':'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({email})
+        }).then(res => res.json())
+        .then(() => {
+            this.setState({
+                email:'',
+                success: true
+            })
+        });
+    }
+
+    clearMessages = () => {
+        setTimeout(function(){
+            this.setState({
+                error: false,
+                success: false
+            })
+        }.bind(this), 3000);
+    }
+
     handleSubmit = (event) => {
        event.preventDefault();
        let email = this.state.email;
+       let regex = /\S+@\S+\.\S+/;  // test email regex
+       if (regex.test(email)) {
+            this.saveSubscription(email);
+       } else {
+            this.setState({error: true});
+       }
+       this.clearMessages();
     }
 
     render() {
@@ -27,6 +63,8 @@ class Subscriptions extends Component {
                 <div>
                     <form onSubmit={this.handleSubmit}>
                         <input name="" type="text" placeholder="Enter your email..." ref="" value={this.state.email} onChange={this.onChangeInput}/>
+                        <div className={this.state.error ? "error show": "error"}>Invalid email!</div>
+                        <div className={this.state.success ? "success show": "success"}>Thank you!</div>
                     </form>
                 </div>
                 <small>NBA freak? Join our newsletter, subscribe to get more information for this stuff blah blah blah basketball.</small>
